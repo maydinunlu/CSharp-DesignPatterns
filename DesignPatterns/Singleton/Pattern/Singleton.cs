c3d213db956826ee950d241228aac3bb96bc847d
maydinunlu <maydinunlu@gmail.com>
2021-03-24 23:33:06 +0300

    Pattern: Singleton

---
+++ DesignPatterns/Singleton/Pattern/Singleton.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.Singleton
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton _instance = new Singleton();
+        public static Singleton Instance => _instance;
+        
+        static Singleton()
+        {
+        }
+
+        private Singleton()
+        {
+        }
+    }
+}
---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DesignPatterns.Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.Instance;
+            Singleton s2 = Singleton.Instance;
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+        }
+    }
+}
