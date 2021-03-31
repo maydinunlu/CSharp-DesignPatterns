21a0a50229e7777cd6e414bc5e84fa433bdaf1cd
maydinunlu <maydinunlu@gmail.com>
2021-03-31 23:39:53 +0300

    Pattern: Decorator

---
+++ DesignPatterns/Decorator/Pattern/ConcreteComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns.Decorator
+{
+    public class ConcreteComponent : Component
+    {
+        public override string Operation()
+        {
+            return "ConcreteComponent";
+        }
+    }
+}
